21f5df95b5ab46edc6b858713d1725680507885f
Seiya <stachizaki71@gmail.com>
2025-07-16 23:31:13 +0900

    モデルの作成

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\BreakTime;
+use App\Models\AttendanceApplication;
 
 class Attendance extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function breakTimes()
+    {
+        return $this->hasMany(BreakTime::class);
+    }
+
+    public function attendanceRequests()
+    {
+        return $this->hasMany(AttendanceApplication::class);
+    }
+
 }
--- src/app/Models/AttendanceApplication.php
+++ src/app/Models/AttendanceApplication.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Attendance;
+use App\Models\AttendanceApplicationItem;
 
 class AttendanceApplication extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(AttendanceApplicationItem::class);
+    }
+
 }
