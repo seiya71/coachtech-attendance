8e72755048c961b900c1e0c66285da86f2ef9cc1
Seiya <stachizaki71@gmail.com>
2025-08-23 10:59:23 +0900

    登録内容と一致しない場合、バリデーションメッセージが表示される機能のテストを作成

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -50,4 +50,23 @@ class LoginTest extends TestCase
             'password' => 'パスワードを入力してください',
         ]);
     }
+
+    /** @test */
+    public function 登録内容と一致しない場合、バリデーションメッセージが表示される()
+    {
+        User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'wrong@example.com',
+            'password' => 'wrongpassword',
+        ]);
+
+        $response->assertSessionHasErrors([
+            'email' => 'ログイン情報が登録されていません',
+        ]);
+    }
+
 }
